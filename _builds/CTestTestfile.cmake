cc730dab18f29003668e085bf37954181960d6ac
SovesT1337 <temirkhan2002@gmail.com>
2021-05-22 16:07:30 +0300

    (^w^)

---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sovest/SovesT1337/workspace/projects/lab07
+# Build directory: /home/sovest/SovesT1337/workspace/projects/lab07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/sovest/SovesT1337/workspace/projects/lab07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/sovest/SovesT1337/workspace/projects/lab07/CMakeLists.txt;60;add_test;/home/sovest/SovesT1337/workspace/projects/lab07/CMakeLists.txt;0;")
